af79cea3fd5cce249e1ba260e4ebadbf3a47dd94
Tamas Heli <helitamas@gmail.com>
2024-02-02 09:10:30 +0100

    Initial db model

--- quiz/quiz_app/models.py
+++ quiz/quiz_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.category_name
+    
+class Question(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    question_text = models.TextField()
+    correct_answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.question_text
+    
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option_text = models.CharField(max_length=255)
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return self.option_text
+    
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+    
+class UserAnswer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    chosen_option = models.ForeignKey(Option, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'question')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.question.question_text}"
+
