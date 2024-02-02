593b51830b1717eb9272561e9b9da59b70bb6448
Tamas Heli <helitamas@gmail.com>
2024-02-02 20:46:01 +0100

    Initial models-views-index

--- quiz/quiz_app/models.py
+++ quiz/quiz_app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.TextField()
+    correct_option = models.CharField(max_length=255)
     option1 = models.CharField(max_length=255)
     option2 = models.CharField(max_length=255)
     option3 = models.CharField(max_length=255)
-    correct_option = models.CharField(max_length=255)
 
     def __str__(self):
         return self.question_text
--- quiz/quiz_app/views.py
+++ quiz/quiz_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from quiz_app.models import Question, User, UserAnswer
 
-# Create your views here.
+
+def index(request):
+    questions = Question.objects.all()
+    return render(request, "index.html", {"questions": questions})
