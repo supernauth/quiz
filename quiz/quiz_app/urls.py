7b1058018d969a144ec9ad3a78e0f39f9181810d
Tamas Heli <helitamas@gmail.com>
2024-02-03 11:01:46 +0100

    Made new migrations, working index.html is ready

--- quiz/quiz_app/urls.py
+++ quiz/quiz_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("question/add", views.add_question),
     path("question/add/addrecord", views.add_question_record),
-    path("question/uptate/<ind:id>", views.update_question),
-    path("question/update/updaterecord/<ind:id>", views.update_question_record),
-    path("question/delete=<ind:id>", views.delete_question),
+    path("question/uptate/<int:id>", views.update_question),
+    path("question/update/updaterecord/<int:id>", views.update_question_record),
+    path("question/delete=<int:id>", views.delete_question),
 ]
