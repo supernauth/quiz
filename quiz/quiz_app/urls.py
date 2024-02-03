6aaf2ec2d38807efa442c05679c724197c222bbf
Tamas Heli <helitamas@gmail.com>
2024-02-03 10:41:14 +0100

    Fixing urls, and index

--- quiz/quiz_app/urls.py
+++ quiz/quiz_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("question", views.add_question),
-    path("question/addrecord", views.add_question_record),
+    path("question/add", views.add_question),
+    path("question/add/addrecord", views.add_question_record),
     path("question/uptate/<ind:id>", views.update_question),
     path("question/update/updaterecord/<ind:id>", views.update_question_record),
     path("question/delete=<ind:id>", views.delete_question),
