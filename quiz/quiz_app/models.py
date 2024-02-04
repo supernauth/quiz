77bdd83c5dc97e9c9ebc12f5cea8c89a10d987ed
Tamas Heli <helitamas@gmail.com>
2024-02-04 21:42:45 +0100

    Db simplification

--- quiz/quiz_app/models.py
+++ quiz/quiz_app/models.py
@@ -3,10 +3,6 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=255)
-    option1 = models.CharField(max_length=255)
-    option2 = models.CharField(max_length=255)
-    option3 = models.CharField(max_length=255)
-    option4 = models.CharField(max_length=255)
     correct_option = models.CharField(max_length=1)
 
     def __str__(self):
